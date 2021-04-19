5ce649f90f8abf9273a835f936f9cfdf95ce5335
Daniel Pape <danielwpape@gmail.com>
2021-04-19 18:06:28 +0100

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Daniel Pape on 19/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
